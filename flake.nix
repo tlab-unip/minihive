79d3b78caeb020123c58388fe109d3239e3de8bf
3waffel <45911671+3waffel@users.noreply.github.com>
2023-12-18 19:08:19 +0000

    use poetry

--- flake.nix
+++ flake.nix
@@ -8,28 +8,11 @@
     pkgs = import nixpkgs {
       system = "x86_64-linux";
     };
-    radb = with pkgs;
-      python3Packages.buildPythonPackage rec {
-        pname = "radb";
-        version = "3.0.5";
-        format = "wheel";
-        src = fetchPypi rec {
-          inherit pname version format;
-          sha256 = "sha256-YnBAE1d2FnziIGD8tclaC6QOCZdIoxKJH7V/fKQt1zo=";
-          dist = python;
-          python = "py3";
-        };
-      };
   in {
     devShells.x86_64-linux = with pkgs; {
       default = mkShell {
         buildInputs = [
-          (python3.withPackages (pyp:
-            with python3Packages; [
-              sqlparse
-              radb
-              antlr4-python3-runtime
-            ]))
+          poetry
         ];
       };
     };
